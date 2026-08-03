ee2a631ceaf63d90b9e5b5bcc9854ac53c0821e5
Tsunemaru <tsumasaki.lock@gmail.com>
2026-08-03 20:30:02 +0900

    Smoke-test deployment images

--- scripts/check-deployment.sh
+++ scripts/check-deployment.sh
@@ -10,7 +10,8 @@ docker build -f deploy/docker/worker.Dockerfile -t ysa-worker:ci .
 docker build -f deploy/docker/gateway.Dockerfile -t ysa-gateway:ci .
 docker build -f deploy/docker/web.Dockerfile -t ysa-web:ci .
 
-docker run --rm ysa-api:ci ./api -help >/dev/null 2>&1 || true
+docker run --rm --entrypoint /bin/sh ysa-api:ci \
+  -c 'test -x ./api && test -x ./migrate && test -x ./m4-demo-report && test -d ../../database/migrations'
 docker run --rm ysa-worker:ci python -c 'import ysa_worker.main'
 docker run --rm ysa-gateway:ci python -c 'import ysa_gateway.app'
 docker run --rm -e YSA_API_ORIGIN=http://api:8080 ysa-web:ci \
